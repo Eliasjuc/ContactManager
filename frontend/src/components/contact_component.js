8557b9594ebe42c08d27a1aefa570140398b0c4b
DCB1287 <zidane_1080@msn.com>
2020-01-23 09:26:56 -0500

    Updated Contacts page
    Got it so the contact data grabbed from database displays for every contact.

--- frontend/src/components/contact_component.js
+++ frontend/src/components/contact_component.js
@@ -2,16 +2,6 @@ import React from 'react'
 import { List, Header, Form, Divider, Button, Modal, Icon } from 'semantic-ui-react'
 import axios from 'axios'
 
-const incomingContact = {
-    name: "Test",
-    cellphone: "Test",
-    homephone: "Test",
-    workphone: "Test",
-    email: "Test"
-}
-
-
-
 async function getContact() {
     try {
 
@@ -36,44 +26,43 @@ async function handleDelete() {
     }
 }
 
-function Contact () {
 
+class Contact extends React.Component {
 
-    return (
-        <>
+    render(){    
+        return (
+            <>
 
-            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"/>
-            <List>
-                <Icon  name='address book' /> <p>Name: {incomingContact.name} </p> 
-                <Icon  name='phone' /> <p>Cell Phone: {incomingContact.cellphone} </p> 
-                <Icon  name='phone' /> <p>Home Phone: {incomingContact.homephone} </p> 
-                <Icon  name='phone' /> <p>Work Phone: {incomingContact.workphone} </p> 
-                <Icon  name='envelope' /> <p>Email: {incomingContact.email} </p>
-            </List>
-            <Button floated="right" icon="pencil" onClick={handleEdit} /> <br /> <br />
-            <Modal trigger={<Button floated="right" >Edit</Button>} closeIcon>
-                <Header icon='archive' content='Archive Old Messages' />
-                <Modal.Content>
-                <p>
-                    Are you sure you want to delete this contact?
-                </p>
-                </Modal.Content>
-                <Modal.Actions>
-                <Button color='red'>
-                    <Icon name='remove' /> No
-                </Button>
-                <Button color='green' onClick={handleDelete}>
-                    <Icon name='checkmark' /> Yes
-                </Button>
-                </Modal.Actions>    
-            </Modal>
-            <br /><br />
-            <Divider /> 
-        </>
+                <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"/>
+                <List>
+                    <Icon  name='address book' /> <p>Name: {this.props.contact.name} </p> 
+                    <Icon  name='phone' /> <p>Cell Phone: {this.props.contact.cellphone} </p> 
+                    <Icon  name='phone' /> <p>Home Phone: {this.props.contact.homephone} </p> 
+                    <Icon  name='phone' /> <p>Work Phone: {this.props.contact.workphone} </p> 
+                    <Icon  name='envelope' /> <p>Email: {this.props.contact.email} </p>
+                </List>
+                <Button floated="right" icon="pencil" onClick={handleEdit} /> <br /> <br />
+                <Modal trigger={<Button floated="right" >Edit</Button>} closeIcon>
+                    <Header icon='archive' content='Archive Old Messages' />
+                    <Modal.Content>
+                    <p>
+                        Are you sure you want to delete this contact?
+                    </p>
+                    </Modal.Content>
+                    <Modal.Actions>
+                    <Button color='red'>
+                        <Icon name='remove' /> No
+                    </Button>
+                    <Button color='green' onClick={handleDelete}>
+                        <Icon name='checkmark' /> Yes
+                    </Button>
+                    </Modal.Actions>    
+                </Modal>
+                <br /><br />
+                <Divider /> 
+            </>
 
-    )
-
+        )
+    }
 }
-
-
 export default Contact
--- frontend/src/pages/Contacts.js
+++ frontend/src/pages/Contacts.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import Contact from '../components/contact_component'
+import axios from 'axios'
+import { CommentActions } from 'semantic-ui-react'
 
 
-function Contacts () {
 
-
-
-    return (
-        <>
-            <Contact />
-        </>
-    )
+class Contacts extends React.Component {
+    constructor() {
+        super()
+        this.state = {contacts: []}
+    }
+    componentDidMount() {
+        axios.get("http://localhost:3000/contacts") 
+            .then( response => {
+                const contacts = response.data
+                this.setState({ contacts })
+            })
+    }   
+    
+    render(){    
+        let contacts = this.state.contacts
+        console.log(contacts)
+        return (
+            <div>
+                {contacts.map(contact => 
+                    <Contact key={contacts._id}
+                        contact={contact}
+                    />
+                    
+                )}
+            </div>
+        )
+    }
 }
 
 export default Contacts
