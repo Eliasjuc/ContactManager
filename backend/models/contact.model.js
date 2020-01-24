b153a83dc5f93bc03ce574e2c46ca3b2f5dd0627
DCB1287 <zidane_1080@msn.com>
2020-01-24 10:41:19 -0500

    Revert "Trying to deploy"
    This reverts commit 12a92c887ff241b7c19301847e53d040fe42e522.

---
+++ backend/models/contact.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const contactSchema = new Schema({
+    user: {
+        type: String,
+        minlength: 1,
+        require: true,
+    },
+
+    name: {
+        type: String,
+        minlength: 1
+    },   
+    cellphone: {
+        type: String,
+        minlength: 1
+    },
+
+    homephone: {
+        type: String,
+    },
+
+    workphone: {
+        type: String,
+    },
+    
+    email: {
+        type: String,
+    },
+
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+module.exports = Contact;
---
+++ backend/server.js
@@ -0,0 +1,47 @@
+// Imported dependencies: middleware to mongoosedb.
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// Allows the existence of the ".env" file.
+require('dotenv').config();
+
+// Initiates express server and defines a port.
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Middleware, allows us to parse json
+app.use(cors());
+app.use(express.json());
+
+// Need a file in the same dir called ".env"
+const uri = process.env.ATLAS_URI;
+
+// Allows us to connect to mongodb. Flags are to allow
+// use of updated functions.
+mongoose.connect(uri, { useNewUrlParser: true,
+     useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully")})
+
+// Allows router files to be accessed via a URL
+// i.e http://localhost:3000/contacts/add
+const contactsConnection = require('./routers/contactsAPI');
+const userConnection = require('./routers/usersAPI');
+app.use('/contacts', contactsConnection);
+app.use('/users', userConnection);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+});
