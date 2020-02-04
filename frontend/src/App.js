bcc4e84ea5fd26e22bad11eeae67716498089c33
Eliasjuc <59970281+Eliasjuc@users.noreply.github.com>
2020-02-03 22:47:25 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,56 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import { Container } from 'semantic-ui-react'
+import { Button, Form, Modal, Segment, Message, Header, Card } from 'semantic-ui-react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import About from './pages/About';
+import cookie from 'js-cookie'
 import Contacts from './pages/Contacts'
 
-function App() 
-{
-
+function App() {
   return (
-    <>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
-    <Container>
+ <>
+<nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark" > 
+
+<a className="navbar-brand" href="#">Contact Manager</a>
+<div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="/Home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/About">About</a>     
+      </li>
+    </ul>
+  
+<button class="ui primary button" onClick = {handleLogout}>       
+Logout
+</button>  
+  </div>   
+  </nav> 
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
+
+    
+
     <Router >
       <Switch>
         <Route path="/" exact>
+          <Homepage />
+          
+        </Route>
+
+        <Route path="/Login" exact >
+        
           <Login />
         </Route>
+
+        <Route path="/About" exact>
+          <About />
+        </Route>
+        
         <Route path="/signup" exact>
           <Signup />
         </Route>
@@ -26,9 +60,20 @@ function App()
         <Redirect to="/" />        
       </Switch>  
     </Router>
-    </Container>
+    
     </>
+    
   );
+  
 }
 
+function handleLogout(){
+cookie.remove("token");
+window.location.href = '.Login'
+}
+
+ 
+
+
 export default App;
+
