272ff5a3795d4cb52bc02f055324cea90976e7e8
Eliasjuc <59970281+Eliasjuc@users.noreply.github.com>
2020-02-03 22:42:25 -0500

    Create About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import {Button,Container,Divider,Grid,Header,Icon,Image,List,Menu,Responsive,Segment,Sidebar,Visibility,} from 'semantic-ui-react'
+import Link from "next/link";
+
+function About(){
+    return(
+        <div class="ui sizer vertical segment">
+  <div class="ui huge header">I still need to create this page</div>
+  
+</div>
+    )
+    
+}
+
+export default About
